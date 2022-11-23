b53e36a24ba8a6a9c5e34f2ee0b867c24083a5dc
Shaq-IB <rahmanshaqib@gmail.com>
2022-11-23 12:46:29 -0500

    mute & deafaz done

--- Deafen_app/app/src/main/java/com/example/deafen_prototype/MainActivity.java
+++ Deafen_app/app/src/main/java/com/example/deafen_prototype/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.deafen_prototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    public void Deafen(View view){
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 1, AudioManager.FLAG_SHOW_UI);
+        Toast.makeText(this,"Deafening", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Mute(View view){
+        audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI);
+        Toast.makeText(this,"Muting", Toast.LENGTH_SHORT).show();
     }
 }
