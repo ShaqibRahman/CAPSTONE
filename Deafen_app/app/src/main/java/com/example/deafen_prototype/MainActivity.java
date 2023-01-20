3c0cc62116c10822e4e8233fcfbc1a6e78197c9d
Shaq-IB <rahmanshaqib@gmail.com>
2023-01-19 21:15:43 -0500

    updated settings UI

--- Deafen_app/app/src/main/java/com/example/deafen_prototype/MainActivity.java
+++ Deafen_app/app/src/main/java/com/example/deafen_prototype/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.deafen_prototype;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
 
                 int volume_level= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);  //captures volume prior to change
-                if (volume_level > 5){
+                if (volume_level == 15){
                     audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI); //mutes volume
                 }
                 handler.postDelayed(this,2000);
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void Deafen(View view){
         int volume_level= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);  //captures volume prior to change
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 1, AudioManager.FLAG_SHOW_UI);  //lowers volume to 1
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 5, AudioManager.FLAG_SHOW_UI);  //lowers volume to 1
         Toast.makeText(this,"Deafening", Toast.LENGTH_SHORT).show();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -73,8 +74,52 @@ public class MainActivity extends AppCompatActivity {
         }, 3000);
     }
 
+
+    public void Trigger(View view) {
+
+        Bundle extras = getIntent().getExtras();
+        Boolean state = null;
+        if (extras != null) {
+            state = extras.getBoolean("state");
+        }
+        if (state == true) {
+            DeafenTrigger();
+        } else {
+            MuteTrigger();
+        }
+    }
+
+    public void DeafenTrigger(){
+        int volume_level= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);  //captures volume prior to change
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 5, AudioManager.FLAG_SHOW_UI);  //lowers volume to 1
+        Toast.makeText(this,"Deafening", Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 3s = 3000ms
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume_level, AudioManager.FLAG_SHOW_UI);
+            }
+        }, 3000);
+    }
+
+    public void MuteTrigger(){
+        int volume_level= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI); //mutes volume
+        Toast.makeText(this,"Muting", Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 3s = 3000ms
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume_level, AudioManager.FLAG_SHOW_UI);
+            }
+        }, 3000);
+    }
+
     public void OpenSettings(){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
 }
--- Deafen_app/app/src/main/java/com/example/deafen_prototype/SettingsActivity.java
+++ Deafen_app/app/src/main/java/com/example/deafen_prototype/SettingsActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageButton backbutton;
+    private SeekBar volume;
+    private Switch action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,25 @@ public class SettingsActivity extends AppCompatActivity {
                 OpenMain();
             }
         });
+
+        volume = (SeekBar) findViewById(R.id.Deafen_volume);
+        volume.setMax(15);
+        volume.setProgress(5);
+
+        action = (Switch)  findViewById(R.id.action_switch);
+        action.setChecked(true);
+
     }
 
     public void OpenMain(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("state", CheckAction());
         startActivity(intent);
     }
+
+    public boolean CheckAction(){
+
+        return action.isChecked();
+    }
+
 }
