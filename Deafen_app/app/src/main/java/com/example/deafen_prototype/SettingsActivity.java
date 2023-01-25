4466d2a6c118d29aca408e98ed38fb4461cd6ae7
Shaq-IB <rahmanshaqib@gmail.com>
2023-01-25 13:37:53 -0500

    fixed issue where settings state does not save

--- Deafen_app/app/src/main/java/com/example/deafen_prototype/SettingsActivity.java
+++ Deafen_app/app/src/main/java/com/example/deafen_prototype/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.deafen_prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -22,23 +23,30 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_settings);
+        SharedPreferences sharedPreferences=getSharedPreferences("save",MODE_PRIVATE);
 
         backbutton = (ImageButton) findViewById(R.id.BackButton);
         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 OpenMain();
             }
         });
 
         volume = (SeekBar) findViewById(R.id.Deafen_volume);
         volume.setMax(15);
+        volume.setProgress(sharedPreferences.getInt("volume", 0));
+
         volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue = 5;
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progressChangedValue = progress;
+                SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                editor.putInt("volume", progress);
+                editor.apply();
+                volume.setProgress(progress);
                 new_volume = progress;
 
             }
@@ -49,19 +57,22 @@ public class SettingsActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(SettingsActivity.this, "Volume bar progress is :" + progressChangedValue,
+                Toast.makeText(SettingsActivity.this, "Volume bar progress is :" + new_volume,
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         time = (SeekBar) findViewById(R.id.timeBar);
         time.setMax(10);
+        time.setProgress(sharedPreferences.getInt("time", 0));
 
         time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int timeChangedValue = 5;
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                timeChangedValue = progress;
+                SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                editor.putInt("time", progress);
+                editor.apply();
+                time.setProgress(progress);
                 new_time = progress;
 
             }
@@ -72,16 +83,51 @@ public class SettingsActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(SettingsActivity.this, "Time bar progress is :" + timeChangedValue,
+                Toast.makeText(SettingsActivity.this, "Time bar progress is :" + new_time,
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         action = (Switch)  findViewById(R.id.action_switch);
 
+        action.setChecked(sharedPreferences.getBoolean("value",true));
+
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (action.isChecked())
+                {
+                    // When switch checked
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    action.setChecked(true);
+                }
+                else
+                {
+                    // When switch unchecked
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    action.setChecked(false);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putInt("volume", new_volume);
+        outState.putInt("time", new_time);
+        outState.putBoolean("state", CheckAction());
+        super.onSaveInstanceState(outState);
+
     }
 
     public void OpenMain(){
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("state", CheckAction());
         intent.putExtra("reduced_vol", new_volume);
