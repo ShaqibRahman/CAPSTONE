40463b2d8319c35cdd3c4b51fcc1b7203f330e21
Nijash Sooriya <44658563+nijashsooriya@users.noreply.github.com>
2023-03-21 16:52:33 -0400

    Add files via upload

--- Deafen_app/app/src/main/java/com/example/deafen_prototype/RecordingService.java
+++ Deafen_app/app/src/main/java/com/example/deafen_prototype/RecordingService.java
@@ -1,36 +1,75 @@
 package com.example.deafen_prototype;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.tensorflow.lite.support.audio.TensorAudio;
+import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
+import org.tensorflow.lite.task.audio.classifier.Classifications;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimerTask;
 
 public class RecordingService extends Service {
 
     private static int DEAFEN_FLAG = 0;
-    private static int THRESHOLD = 127;
+    private static int THRESHOLD = 180;
+    private TensorAudio tensorAudio;
+    private AudioClassifier audioClassifier;
+    boolean state;
+    int settings_volume;
+    int settings_time;
+    ArrayList<String> container;
 
     @Override
     //service starts on this method
     public int onStartCommand(Intent intent, int flags, int startId) {
         //any start up sequence required goes here
         //Toast.makeText(this,"Recording Service Initialized", Toast.LENGTH_SHORT).show();
+        Log.i("recording", "recording service initialized");
+        audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
         startRecording();
+
+        Bundle extras = intent.getExtras();
+        settings_time = extras.getInt("deafen_time"); //gets the time preference of the user from the settings
+        settings_volume = extras.getInt("reduced_vol");
+        state = extras.getBoolean("state");
+        String modelPath = "lite-model_yamnet_classification_tflite_1.tflite";
+        try {
+            audioClassifier = AudioClassifier.createFromFile(this, modelPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return START_STICKY;
     }
 
     @Override
     //delete
     public void onDestroy() {
+        recorder.release();
         super.onDestroy();
     }
 
@@ -73,18 +112,145 @@ public class RecordingService extends Service {
         }
     }
 
-    private void processAudio(short[] data){
+
+    private void processAudio(short[] data) { //edit this function to change audio functionality
         short max = shortMax(data);
-
-        if(max>THRESHOLD){
-            DEAFEN_FLAG  = 1;
-        }
-
-        else{
-            DEAFEN_FLAG = 0;
+        // call classification here
+        int volume_level = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if (max > THRESHOLD) {
+            if (DEAFEN_FLAG == 0) {
+                DEAFEN_FLAG = 1;
+                flagZeroAction();
+            }
+        } else {
+            if (DEAFEN_FLAG == 1) {
+                DEAFEN_FLAG = 0;
+                flagOneAction(volume_level);
+            }
         }
     }
 
+    //processAudioML(short[] data){ invoke classifier within this and then check if inferences match up , try and implement bool
+    private boolean processAudioML(short[] data) {
+        short max = shortMax(data);
+        tensorAudio = audioClassifier.createInputTensorAudio();
+        tensorAudio.load(data);
+        ArrayList<String> model_output = classification(tensorAudio);
+        ArrayList<String> reference_array  = new ArrayList<>();
+        //Sound matching population used in the next step
+        //Cleaner to do one line adds than to do a pan-by-pan one liner
+        reference_array.add("Vehicle");
+        reference_array.add("Emergency Vehicle");
+        reference_array.add("Police car (siren)");
+        reference_array.add("Fire engine, fire truck (siren)");
+        reference_array.add("Sine wave");
+        reference_array.add("Ambulance (siren)");
+        reference_array.add("Vehicle horn, car horn, honking");
+
+
+        for(int i = 0; i < model_output.size(); i++){
+            if(reference_array.contains(model_output.get(i))){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private ArrayList<String> classification(TensorAudio tensor){
+
+        List<Classifications> classifier  = audioClassifier.classify(tensor);
+        //Running the inferences
+        ArrayList<String> outputFinal = new ArrayList<>();
+        for(Classifications classifications: classifier){
+            for(Category category: classifications.getCategories()){
+                if(category.getScore() > 0.3f){
+                    outputFinal.add(category.getDisplayName());
+                }
+
+            }
+        }
+
+
+        //Iterate through final output, use container.add(outputFinal[i])
+
+
+        return outputFinal;
+
+    }
+
+
+
+    private void flagZeroAction(){
+
+        //this is triggered when the flag goes from 1 to 0
+
+        if (state == true) {
+            DeafenTrigger();
+        } else {
+            PauseTrigger();
+        }
+    }
+
+    private void flagOneAction(int volume){
+
+        //this is triggered when the flag goes from 0 to 1
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 3s = 3000ms
+                if(DEAFEN_FLAG==0){
+
+
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, AudioManager.FLAG_SHOW_UI);
+                    
+
+                }
+            }
+        }, settings_time*1000);
+    }
+
+    public void DeafenTrigger(){
+
+
+        int volume_level = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);  //captures volume prior to change
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, settings_volume, AudioManager.FLAG_SHOW_UI);  //lowers volume to 1
+
+        Toast.makeText(this,"Deafening", Toast.LENGTH_SHORT).show();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 3s = 3000ms
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume_level, AudioManager.FLAG_SHOW_UI);
+            }
+        }, settings_time*1000);
+    }
+
+
+    public void PauseTrigger(){
+
+        Context context = this;
+        // stop music player
+        AudioManager pause_play = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        pause_play.requestAudioFocus(null,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+        Toast.makeText(this,"Pausing", Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //resume music player after a few seconds
+                pause_play.abandonAudioFocus(null);
+            }
+        }, settings_time*1000);
+    }
+
+
+    @SuppressLint("MissingPermission")
     private void startRecording() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -125,6 +291,11 @@ public class RecordingService extends Service {
 
     }
 
+    private void endRecording(){ //make this better?
+        recorder.release();
+
+    }
+
     private short shortMax(short[] data){
         short max = 0;
         for (int i = 0; i < data.length; i++) {
@@ -138,5 +309,8 @@ public class RecordingService extends Service {
     public static int getDeafenFlag(){
         return DEAFEN_FLAG;
     }
+    AudioManager audioManager;
+
+
 }
 
